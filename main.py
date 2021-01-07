05539bc75d309980474e327171180bef25f95313
massudavide <massu.davide@gmail.com>
2021-01-07 13:38:38 +0100

    creato shingles per tutte le pagine web in cartella e sottocartella

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import shinglesCreator as sc
 import langFinder as lf
 import numpy as np
+import pandas as pd
 
 def main():
     # lf.lista_pagine_web()
-    sc.creazione_matrice_caratteristica()
+    # sc.creazione_matrice_caratteristica()
+    # sc.lista_pagine_web()
+
+    # df = pd.read_csv('dfShingles.csv')
+    # print(df.columns)
 
 if __name__ == "__main__":
     main()
--- shinglesCreator.py
+++ shinglesCreator.py
@@ -9,7 +9,8 @@ import numpy as np
 def creazione_matrice_caratteristica():
     df_shingles = creazione_matrice_caratteristica_aux()
     df_shingles = df_shingles.reset_index()
-    print(df_shingles.columns)
+    df_shingles.to_csv('dfShingles.csv', index=False)
+    print(df_shingles)
 
 def creazione_matrice_caratteristica_aux():
     shingles_df = pd.DataFrame([], [])
@@ -21,6 +22,8 @@ def creazione_matrice_caratteristica_aux():
 
 def concat_df(pagina_web, df):
     shingle_list = shingle_set(pagina_web)
+    if len(shingle_list) == 0:
+        return df
     df2 = pd.DataFrame(np.ones(len(shingle_list), dtype=int), columns=[pagina_web], index=shingle_list)
     df = pd.concat([df, df2], axis=1, join="outer")
     return df
@@ -28,10 +31,29 @@ def concat_df(pagina_web, df):
 
 def lista_pagine_web():
     lista_pagine = []
-    os.chdir("webPages/www.themoviedb.org")
-    for file in glob.glob("*/*.html"):
+    rootdir = 'webPages/www.themoviedb.org'
+    for file in glob.glob(f'{rootdir}/**/*.html', recursive=True):
         lista_pagine.append(file)
+    # print(lista_pagine)
     return lista_pagine
+
+# def lista_pagine_web():
+#     lista_pagine = []
+#     rootdir = 'webPages/www.themoviedb.org'
+#     for file in glob.glob(f'webPages/www.themoviedb.org\\assets\\2\\favicon-32x32-543a21832c8931d3494a68881f6afcafc58e96c5d324345377f3197a37b367b5.html'):
+#         lista_pagine.append(file)
+#     # print(lista_pagine)
+#     return lista_pagine
+
+
+# funzione deprecata perchè non scende nei sotto alberi (sotto cartelle)
+# def lista_pagine_web():
+#     lista_pagine = []
+#     os.chdir("webPages/www.themoviedb.org")
+#     for file in glob.glob("*/*.html"):
+#         lista_pagine.append(file)
+#     print(lista_pagine)
+#     return lista_pagine
 
 def shingle_set(pagina_web):
     tag_list = regex(pagina_web)
@@ -50,6 +72,8 @@ def list_of_shingle(sequence_of_tag):
 def tag_slider(tag_list, bucket_size=10, overlap_count=9):
     window_tag = []
     tag_array = np.array(tag_list)
+    if len(tag_array) < bucket_size:
+        return [tag_array]
     slider = Slider(bucket_size, overlap_count)
     slider.fit(tag_array)
     while True:
@@ -64,7 +88,11 @@ def remove_empty_tag(string):
     return string!="" and not string.startswith('!')
 
 def regex(pagina_web):
-    s = codecs.open(pagina_web, "r", "utf-8").read()
+    try:
+        s = codecs.open(pagina_web, "r", "utf-8", errors='ignore').read()
+    except:
+        return []
+    # s = codecs.open(pagina_web, "r", "utf-8", errors='ignore').read()
     all_tag = re.findall('<.*?>', s)
 
     words_list = list(map(clean_tag, all_tag))
