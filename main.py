8b546a9e43779d019b11f89237e738c0ce605f53
massudavide <massu.davide@gmail.com>
2021-01-14 18:19:54 +0100

    bux fixing

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ def main():
 
 def valutazione(mv_list):
     for i in range(len(mv_list)):
-        print('-----', mv_list[i][0], '-----', '\n')
         if mv_list[i][3] > 0:
+            print('-----', mv_list[i][0], '-----', '\n')
             dict_value_cluster = {}
             for j in range(len(mv_list[i][2])):
                 word = mv_list[i][2][j].split("\\")
